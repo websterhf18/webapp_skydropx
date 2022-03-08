4341f37e2336734a8036c7d7ae5d5b7163862803
websterhf18 <websterh18@gmail.com>
2022-03-08 00:48:32 -0500

    Keep calm and commit

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    tabWidth: 2,
+    arrowParens: "always",
+    semi: true,
+    trailingComma: "all",
+    singleQuote: false,
+    useTabs: false,
+    bracketSpacing: true,
+    htmlWhitespaceSensitivity: "css",
+    insertPragma: false,
+    jsxSingleQuote: false,
+    printWidth: 80,
+    proseWrap: "preserve",
+    quoteProps: "as-needed",
+};
+  
